6b43457f750e6bd088d9b655dbec5024bcb9db01
Laura Enria <enria.laura2@gmail.com>
2021-01-26 14:30:52 +0000

    add action login, views login1 and corrispective  routes

--- app/controllers/TaskList1.scala
+++ app/controllers/TaskList1.scala
@@ -7,6 +7,15 @@ import play.api.i18n._
 
 @Singleton
 class TaskList1 @Inject()(val cc: ControllerComponents) extends AbstractController(cc) {
+
+  def login: Action[AnyContent] = Action {
+    Ok(views.html.login1())
+  }
+
+  def validateLoginGet(username: String, password: String): Action[AnyContent] = Action {
+    Ok(s"$username logged in with $password.")
+  }
+
   def taskList: Action[AnyContent] = Action {
     val tasks = List("Coffee", "Tea", "Milk")
     Ok(views.html.taskList1(tasks))
