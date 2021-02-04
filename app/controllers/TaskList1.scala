be13c5432d091bbfa21bcd9785fb215b5c4b3402
Laura Enria <enria.laura2@gmail.com>
2021-02-04 11:51:07 +0000

    add: validateLoginPost

--- app/controllers/TaskList1.scala
+++ app/controllers/TaskList1.scala
@@ -16,8 +16,18 @@ class TaskList1 @Inject()(val cc: ControllerComponents) extends AbstractControll
     Ok(s"$username logged in with $password.")
   }
 
+  def validateLoginPost: Action[AnyContent] = Action { request =>
+    val postVals: Option[Map[String, Seq[String]]] = request.body.asFormUrlEncoded
+    postVals.map { args =>
+      val username: Seq[String] = args("username")
+      val password: Seq[String] = args("password")
+      Ok(s"$username logged in with $password.")
+    }.getOrElse(
+      Ok(s"oop"))
+  }
+
   def taskList: Action[AnyContent] = Action {
-    val tasks = List("Coffee", "Tea", "Milk")
+    val tasks: List[String] = List("Coffee", "Tea", "Milk")
     Ok(views.html.taskList1(tasks))
   }
 }
